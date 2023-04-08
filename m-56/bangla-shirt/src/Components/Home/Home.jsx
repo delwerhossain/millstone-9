788ef15e74cba479436d2484dd35e0163ad72bb3
delwerHossain <delwerhossain006@gmail.com>
2023-04-08 12:34:51 +0100

    Context Api added

--- m-56/bangla-shirt/src/Components/Home/Home.jsx
+++ m-56/bangla-shirt/src/Components/Home/Home.jsx
@@ -1,50 +1,63 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Products from '../Products/Products';
-import Carts from '../Carts/Carts';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Products from "../Products/Products";
+import Carts from "../Carts/Carts";
+import { createContext } from "react";
+import Order from "../Order/Order";
+
+export const LevelContext = createContext(0);
 
 const Home = () => {
-    const products = useLoaderData()
-    const [cartProducts , setCartProducts] = useState([])
-    const handleCarts = (id) => {
-        // const addedProduct = []
-        const exist = cartProducts.find(product => product._id === id._id)
-        if (exist) {
-            alert("Product already")
-        }
-        else {
-            setCartProducts([...cartProducts,id ])
-        }
+  const products = useLoaderData();
+  const [cartProducts, setCartProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const handleCarts = (id) => {
+    // const addedProduct = []
+    const exist = cartProducts.find((product) => product._id === id._id);
+    if (exist) {
+      alert("Product already");
+    } else {
+      setCartProducts([...cartProducts, id]);
     }
+  };
 
-    const handleRemove = (id) => {
-        const remove = cartProducts.filter(product => product._id !== id)
-        setCartProducts([...remove])
-     }
+  const handleRemove = (id) => {
+    const remove = cartProducts.filter((product) => product._id !== id);
+    setCartProducts([...remove]);
+  };
 
-
-
-    return (
-        <div className='grid lg:grid-cols-4 grid-cols-1 mt-6 '>
-            <div className='col-span-1 lg:col-span-3'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:mx-4'>
-              {products.map(product => <Products key={product._id} product={product} handleCarts={handleCarts}></Products>)}
+  return (
+    <LevelContext.Provider value={[count, setCount]}>
+      <div className="grid lg:grid-cols-4 grid-cols-1 mt-6 ">
+        <div className="col-span-1 lg:col-span-3">
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:mx-4">
+                      <button onClick={()=>setCount(count+1)} className="btn">counter </button>
+            {products.map((product) => (
+              <Products
+                key={product._id}
+                product={product}
+                handleCarts={handleCarts}
+              ></Products>
+            ))}
+          </div>
         </div>
-            </div>
-            {/* order card */}
-            <div className='col-span-1 rounded-2xl lg:my-0 my-4 bg-orange-300 h-full
-             '>
-                 <div className=' lg:fixed lg:my-0 my-4'>
-            <h1 className='text-6xl'> cartProducts : {cartProducts.length}</h1>
-                    {cartProducts.map(pd => <Carts key={pd._id
-                    } pd={pd} handleRemove={handleRemove} ></Carts>)}
-            
-            
+        {/* order card */}
+        <div
+          className="col-span-1 rounded-2xl lg:my-0 my-4 bg-orange-300 h-full
+             "
+        >
+          <div className=" lg:fixed lg:my-0 my-4">
+            <h1 className="text-6xl"> cartProducts : {cartProducts.length}</h1>
+            {cartProducts.map((pd) => (
+              <Carts key={pd._id} pd={pd} handleRemove={handleRemove}></Carts>
+            ))}
+                      
+                <Order></Order>
+          </div>
         </div>
-               
-            </div>
-       </div>
-    );
+      </div>
+    </LevelContext.Provider>
+  );
 };
 
 export default Home;
--- m-56/bangla-shirt/src/Components/Order/Order.jsx
+++ m-56/bangla-shirt/src/Components/Order/Order.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LevelContext } from '../Home/Home';
 
 const Order = () => {
+    const [count ,serCount] = useContext(LevelContext)
+   
     return (
+
         <div>
-            <h1 className='bg-red-500 w-full text-5xl'> form order</h1>
+            <h1 className='bg-red-500 w-full text-5xl'> form order count : {count}</h1>
+
         </div>
     );
 };
